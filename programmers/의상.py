b3b14fb49de423704a7df556c699cdbf0ca07520
Juxhee <60679596+Juxhee@users.noreply.github.com>
2024-03-13 23:07:56 +0900

    Update 의상.py

--- programmers/의상.py
+++ programmers/의상.py
@@ -1,6 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42578
+# 프로그래머스 의상
+
 def solution(clothes):
     dict = {}
-    # 해시테이블로 카테고리별 의상 담아두는게 포인트
+    # 해시테이블로 카테고리별 의상 담아두는게 포인트!
     for i,j in clothes:
         if j in dict.keys():
             dict[j] += [i]
